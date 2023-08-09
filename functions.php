8304474b99b144d523690c46c867f24ed11fc5c8
Alexei Samarschi <samarschi@gmail.com>
2023-08-09 15:21:11 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -324,7 +324,7 @@ function inspiro_blocks_get_fonts_url() {
 
 	if( !empty( $user_custom_typos ) ) {
 		foreach( $user_custom_typos as $user_custom_typo ) {
-			$fonts_to_download[] = $font_families[ $user_custom_typo ];
+			$fonts_to_download[] = isset( $font_families[ $user_custom_typo ] ) ? $font_families[ $user_custom_typo ] : '';
 		}
 
 		$query_args = array(
